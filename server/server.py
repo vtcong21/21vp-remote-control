61b5b1d84b76ae14b17f1404fb541040b4879aad
vtcong21 <vtcong21@vp.fitus.edu.vn>
2023-08-31 00:59:13 +0700

    done registry server

--- server/server.py
+++ server/server.py
@@ -1,5 +1,5 @@
 import io
-import csv
+import os
 import json
 import psutil
 import socket
@@ -15,7 +15,8 @@ class Server:
         self.server_ip = ""
         self.socket = None
         self.running = False
-        self.keylogger = Keylogger()
+        self.cache_path = "tmp_files"
+        self.keylogger = Keylogger(os.path.join(self.cache_path), "keylog.txt")
         self.client_threads = []
 
     def stop_server(self):
@@ -107,8 +108,134 @@ class Server:
                 return f"Started application: {app_name}"
             except ValueError:
                 return "Invalid application name."
+        elif request == "registry patch":
+            response = self.handle_registry_patch(client_socket)
+            return response
+        elif request.startswith("get"):
+            try:
+                _, key_path, value_name = request.split(" ", 2)  # Tách lệnh, đường dẫn, và tên giá trị
+                result = self.get_registry_value(key_path, value_name)
+                return result
+            except ValueError:
+                return "Invalid get request."
+        elif request.startswith("set"):
+            try:
+                _, key_path, value_name, value_data, value_type = request.split(" ", 4)  # Tách lệnh, đường dẫn, tên giá trị, dữ liệu giá trị, và kiểu giá trị
+                result = self.set_registry_value(key_path, value_name, value_data, value_type)
+                return result
+            except ValueError:
+                return "Invalid set request."
+        elif request.startswith("delete"):
+            try:
+                _, key_path, value_name = request.split(" ", 2)  # Tách lệnh, đường dẫn, và tên giá trị
+                result = self.delete_registry_value(key_path, value_name)
+                return result
+            except ValueError:
+                return "Invalid delete request."
+        elif request.startswith("create_key"):
+            try:
+                _, key_path = request.split(" ", 1)  # Tách lệnh và đường dẫn khóa
+                result = self.create_registry_key(key_path)
+                return result
+            except ValueError:
+                return "Invalid create_key request."
+        elif request.startswith("delete_key"):
+            try:
+                _, key_path = request.split(" ", 1)  # Tách lệnh và đường dẫn khóa
+                result = self.delete_registry_key(key_path)
+                return result
+            except ValueError:
+                return "Invalid delete_key request."
         else:
             return "Invalid request."
+    #create, delete key registry
+    def create_registry_key(self, key_path):
+        try:
+            # Sử dụng subprocess để chạy reg add để tạo khóa trong Registry
+            command = f'reg add "{key_path}" /f'
+            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            if result.returncode == 0:
+                return f"Registry key {key_path} created successfully."
+            else:
+                return result.stderr
+        except Exception as e:
+            return f"Error creating registry key: {str(e)}"
+
+    def delete_registry_key(self, key_path):
+        try:
+            # Sử dụng subprocess để chạy reg delete để xóa khóa khỏi Registry
+            command = f'reg delete "{key_path}" /f'
+            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            if result.returncode == 0:
+                return f"Registry key {key_path} deleted successfully."
+            else:
+                return result.stderr
+        except Exception as e:
+            return f"Error deleting registry key: {str(e)}"
+    #get,setcreate value registry
+    def get_registry_value(self, key_path, value_name):
+        try:
+            # Sử dụng subprocess để chạy reg query để lấy giá trị từ Registry
+            command = f'reg query "{key_path}" /v "{value_name}"'
+            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            if result.returncode == 0:
+                return result.stdout
+            else:
+                return result.stderr
+        except Exception as e:
+            return f"Error getting registry value: {str(e)}"
+
+    def set_registry_value(self, key_path, value_name, value_data, value_type):
+        try:
+            # Sử dụng subprocess để chạy reg add để thiết lập giá trị trong Registry
+            command = f'reg add "{key_path}" /v "{value_name}" /d "{value_data}" /t {value_type} /f'
+            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            if result.returncode == 0:
+                return f"Registry value {value_name} set successfully."
+            else:
+                return result.stderr
+        except Exception as e:
+            return f"Error setting registry value: {str(e)}"
+
+    def delete_registry_value(self, key_path, value_name):
+        try:
+            # Sử dụng subprocess để chạy reg delete để xóa giá trị khỏi Registry
+            command = f'reg delete "{key_path}" /v "{value_name}" /f'
+            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+            if result.returncode == 0:
+                return f"Registry value {value_name} deleted successfully."
+            else:
+                return result.stderr
+        except Exception as e:
+            return f"Error deleting registry value: {str(e)}"
+    # handle registry patch
+    def handle_registry_patch(self, client_socket):
+        try:
+            print("Đang nhận dữ liệu registry patch từ client...")
+            registry_data = b""
+
+            # Nhận và ghi dữ liệu từ client
+            while True:
+                chunk = client_socket.recv(1024)
+                if not chunk:
+                    break
+                registry_data += chunk
+
+            # Lưu dữ liệu vào tệp tin registry
+            with open(os.path.join(self.cache_path, "registry.reg"), "wb") as reg_file:
+                reg_file.write(registry_data)
+
+            print("Đã nhận và lưu registry patch thành công.")
+            return "Registry patch applied successfully."
+
+        except Exception as e:
+            print(f"Lỗi xử lý registry patch: {str(e)}")
+            return f"Error applying registry patch: {str(e)}"
 
     # kill process
     def kill(self, pid):
@@ -185,19 +312,27 @@ class Server:
 
     def handle_processus_request(self, client_socket):
         try:
-            processes = self.get_running_processus()
-            response = json.dumps(processes)
+            print("Đang nhận dữ liệu registry patch từ client...")
+            registry_data = b""
 
-            # Chia dữ liệu thành các gói tin có kích thước nhỏ
-            chunk_size = 1024  # Kích thước mỗi gói tin
-            chunks = [response[i:i+chunk_size] for i in range(0, len(response), chunk_size)]
+            # Nhận và ghi dữ liệu từ client
+            while True:
+                chunk = client_socket.recv(1024)
+                if not chunk:
+                    break
+                registry_data += chunk
 
-            # Gửi từng gói tin cho client
-            for chunk in chunks:
-                self.send_packet(client_socket, chunk)
+            # Lưu dữ liệu vào tệp tin registry
+            reg_file_path = os.path.join(self.cache_path, "registry.reg")
+            with open(reg_file_path, "wb") as reg_file:
+                reg_file.write(registry_data)
 
-            # Gửi tín hiệu "done" để đánh dấu kết thúc dữ liệu
-            self.send_packet(client_socket, "done")
+            print("Đã nhận và lưu registry patch thành công.")
+
+            # Sử dụng subprocess để chạy regedit và áp dụng thay đổi
+            subprocess.Popen(['regedit', '/s', reg_file_path], shell=True)
+
+            return "Registry patch applied successfully."
 
         except Exception as e:
             error_message = str(e)
@@ -211,6 +346,14 @@ class Server:
             client_socket.send(packet.encode())
         except Exception as e:
             print(f"Error sending packet: {str(e)}")
+
+    def registry_patch(self, reg_file_path):
+        try:
+            subprocess.Popen(['regedit', '/s', reg_file_path], shell=True)
+            return "Registry patch applied successfully."
+        except Exception as e:
+            return f"Error applying registry patch: {str(e)}"
+
 
 if __name__ == "__main__":
     server = Server()

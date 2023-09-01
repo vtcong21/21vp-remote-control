17b06d45a2cc9142012ef06e7dbfdca48a9a2297
xuanuyenvu <xuanuyenvu22@gmail.com>
2023-09-01 18:11:19 +0700

    registry ui

--- client/registry_ui.py
+++ client/registry_ui.py
@@ -10,7 +10,7 @@ class RegistryUI(Registry):
 
     def setup_ui(self):
         self.top_window = tk.Toplevel(self.window)
-        self.top_window.geometry("400x500")
+        self.top_window.geometry("400x360")
         self.top_window.title("Registry Operations")
 
         # Apply the "arc" theme to the top window
@@ -18,7 +18,7 @@ class RegistryUI(Registry):
         style.set_theme("arc")
 
         # Create dropdown menu for selecting function
-        functions = ["Get Value", "Set Value", "Delete Value", "Create Key", "Delete Key"]
+        functions = ["Get Value", "Get Value", "Set Value", "Delete Value", "Create Key", "Delete Key"]
         self.selected_function = tk.StringVar()
         self.selected_function.set(functions[0])  # Set default function
 
@@ -31,33 +31,31 @@ class RegistryUI(Registry):
    
         self.label_value_name = self.create_label("Value Name:")
         self.entry_value_name = self.create_entry()
-        self.entry_value_name.pack_forget()  # Ẩn mặc định
+        self.entry_value_name.place_forget()  # Ẩn mặc định
 
         self.label_value_data = self.create_label("Value Data:")
         self.entry_value_data = self.create_entry()
-        self.entry_value_data.pack_forget()  # Ẩn mặc định
+        self.entry_value_data.place_forget()  # Ẩn mặc định
 
         self.label_value_type = self.create_label("Value Type:")
         self.entry_value_type = self.create_entry()
-        self.entry_value_type.pack_forget()  # Ẩn mặc định
+        self.entry_value_type.place_forget()  # Ẩn mặc định
 
         # Create buttons
         self.execute_button = ttk.Button(self.top_window, text="Execute", command=self.execute_function)
-        self.execute_button.place(relx=0.05, rely=0.8, relwidth=0.9)
 
         # Create log text widget
         self.log_text = self.create_text_widget()
-        self.log_text.place(relx=0.05, rely=0.6, relwidth=0.9, relheight=0.2)
         
         # Create scrollbar for the log text widget
         self.scrollbar = self.create_scrollbar()
 
         # Pack widgets
-        self.label_key_path.pack()
-        self.entry_key_path.pack()
-        self.log_text.pack(fill=tk.BOTH, expand=True)
-        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        self.execute_button.pack()
+        self.label_key_path.place(relx=0.05, rely=0.12)
+        self.entry_key_path.place(relx=0.05, rely=0.16, relwidth=0.9)
+        self.log_text.place(relx=0.05, rely=0.38, relwidth=0.9, relheight=0.48)
+        self.scrollbar.place(relx=0.95, rely=0.38, relheight=0.48)
+        self.execute_button.place(relx=0.25, rely=0.88, relwidth=0.5)
 
         # Initialize fields
         self.update_fields()
@@ -66,26 +64,32 @@ class RegistryUI(Registry):
         selected = self.selected_function.get()
 
         if selected == "Get Value":
-            self.hide_fields([self.label_value_data, self.entry_value_data, self.label_value_type, self.entry_value_type])
-            self.show_fields([self.label_value_name, self.entry_value_name])
+            self.fields_1()
         elif selected == "Set Value":
-            self.show_fields([self.label_value_data, self.entry_value_data, self.label_value_type, self.entry_value_type, self.label_value_name, self.entry_value_name])
+            self.fields_2()
         elif selected == "Delete Value":
-            self.hide_fields([self.label_value_data, self.entry_value_data, self.label_value_type, self.entry_value_type])
-            self.show_fields([self.label_value_name, self.entry_value_name])
+            self.fields_1()
         elif selected == "Create Key":
-            self.hide_fields([self.label_value_name, self.entry_value_name, self.label_value_data, self.entry_value_data, self.label_value_type, self.entry_value_type])
+            self.fields_2()
         elif selected == "Delete Key":
-            self.hide_fields([self.label_value_name, self.entry_value_name, self.label_value_data, self.entry_value_data, self.label_value_type, self.entry_value_type])
+            self.fields_2()
 
-    def hide_fields(self, fields):
-        for field in fields:
-            field.pack_forget()
-
-    def show_fields(self, fields):
-        for field in fields:
-            field.pack()
-
+    def fields_1(self):
+        self.label_value_name.place(relx=0.05, rely=0.24, relwidth=0.28)
+        self.entry_value_name.place(relx=0.05, rely=0.28, relwidth=0.28)
+        self.label_value_data.place_forget()
+        self.entry_value_data.place_forget()
+        self.label_value_type.place_forget()
+        self.entry_value_type.place_forget()
+    
+    def fields_2(self):
+        self.label_value_name.place(relx=0.05, rely=0.24, relwidth=0.28)
+        self.entry_value_name.place(relx=0.05, rely=0.28, relwidth=0.28)
+        self.label_value_data.place(relx=0.36, rely=0.24, relwidth=0.28)
+        self.entry_value_data.place(relx=0.36, rely=0.28, relwidth=0.28)
+        self.label_value_type.place(relx=0.67, rely=0.24, relwidth=0.28)
+        self.entry_value_type.place(relx=0.67, rely=0.28, relwidth=0.28)
+        
     def create_label(self, text):
         return ttk.Label(self.top_window, text=text)
 
